f92f939527516c4876ee562660c646ea97516ce5
Daniel Kim <chipperday@gmail.com>
2016-08-15 10:24:36 -0700

    canvas preview init

---
+++ canvas-preview/game.js
@@ -0,0 +1,67 @@
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+
+    this.width = canvas.width;
+    this.height = canvas.height;
+
+    this.procId = -1;
+    this.units = [];
+    
+    this.spawnUnits(10);
+  }
+
+  spawnUnits(num) {
+    for(var i=0; i<num; i++) {
+      var unit = new Unit(this.width/2, this.height/2, this.canvas, this.ctx);
+      this.units.push(unit);
+    }
+  }
+
+  start() {
+    var draw = this.draw.bind(this);
+    setInterval(draw, 1000 / 60);
+  }
+
+  draw() {
+    this.moveUnits();
+    
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.drawGrid();
+    this.drawUnits();
+  }
+  
+  moveUnits() {
+    for(var i=0; i<this.units.length; i++) {
+      this.units[i].move();
+    }
+  }
+  
+  drawUnits() {
+    for(var i=0; i<this.units.length; i++) {
+      this.units[i].draw();
+    }
+  }
+
+  drawGrid() {
+    var ctx = this.ctx;
+    ctx.strokeStyle = 'gray';
+    ctx.lineWidth = 1;
+
+    for (var y = 0; y < this.height; y += this.height / 10) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(this.width, y);
+      ctx.stroke();
+    }
+    
+    for (var x = 0; x < this.width; x += this.width / 10) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, this.height);
+      ctx.stroke();
+    }
+
+  }
+}
