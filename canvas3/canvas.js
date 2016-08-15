94cccf86a6073d1b4ff4930421555829029690b2
Daniel Kim <chipperday@gmail.com>
2016-08-15 00:27:45 -0700

    everything

--- canvas3/canvas.js
+++ canvas3/canvas.js
@@ -1,31 +1,61 @@
+class Circle {
+  constructor(x,y,velX,velY,radius,strokeColor,fillColor,lineWidth,cv,ctx) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.radius = radius;
+    this.strokeColor = strokeColor;
+    this.fillColor = fillColor;
+    this.lineWidth = lineWidth;
+    this.cv = cv;
+    this.ctx = ctx;
+  }
+  draw() {
+    var ctx = this.ctx;
+    var cv = this.cv;
+
+    ctx.strokeStyle=this.strokeColor;
+    ctx.fillStyle=this.fillColor;
+    ctx.lineWidth=this.lineWidth;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+    /  
+  }
+  /*
+  recalculateXY() {
+    if (x+radius >= cv.width ||
+        x-radius <= 0) {
+          velX *= -1;
+        }
+
+      if (y+radius >= cv.height ||
+          y-radius <= 0) {
+            velY *= -1;
+          }
+
+
+    x += velX;
+    y += velY;
+  }*/
+}
+
 var cv = document.querySelector("#cv");
 var ctx = cv.getContext("2d");
 
-var x = 100;
-var y = 100;
-var radius = 50;
-var velX = 5;
-var velY = 5;
-
-function draw() {
-  ctx.clearRect(0, 0, cv.width, cv.height);
-
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, 2*Math.PI);
-  ctx.stroke();
-
-  recalculateXY();
+var circles = [];
+for(var i=0; i<10; i++) {
+  circles[i] = new Circle(10+i, 10+i, 10, 10, 10, 'red','black', 3, cv, ctx);
+  // TODO - randomize color, size, initial velocity....
 }
 
-function recalculateXY() {
-  if (x+radius >= cv.width ||
-      x-radius <= 0) {
-        velX *= -1;
-      }
-
-  x += velX;
-  y += velY;
+function draw() {
+  for(var i=0; i<circles.length; i++) {
+    circles[i].draw();
+  }
 }
 
 setInterval(draw, 1000/60);
---
+++ canvas4/unit.js
@@ -0,0 +1,51 @@
+class Unit {
+    constructor(x, y, canvas, ctx) {
+        this.x = x;
+        this.y = y;
+        this.canvas = canvas;
+        this.ctx = ctx;
+
+        this.velX = this.getRandomVel();
+        this.velY = this.getRandomVel();
+        this.color = this.getRandomColor();
+
+        this.level = 1;
+        this.point = 0;
+
+        this.calculateSize();
+    }
+
+    getRandomVel() {
+        var plusOrMinus = (Math.random() < 0.5) ? -1 : 1;
+        return Math.random() * plusOrMinus;
+    }
+
+    getRandomColor() {
+        return "#" + Math.random().toString(16).slice(2, 8).toUpperCase();
+    }
+
+    calculateSize() {
+        this.radius = 10 + (2 * (this.level - 1));
+        this.border = 0.1 + (0.1 * (this.level - 1));
+    }
+
+    move() {
+        this.x += this.velX;
+        this.y += this.velY;
+
+        this.x = Math.min(Math.max(this.x, 0), this.canvas.width);
+        this.y = Math.min(Math.max(this.y, 0), this.canvas.height);
+    }
+
+    draw() {
+        var ctx = this.ctx;
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = this.border;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+    }
+}
