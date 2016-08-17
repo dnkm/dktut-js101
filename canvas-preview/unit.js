d7db5ed846bb073f3be3aced8ba7dd03918151be
Daniel Kim <chipperday@gmail.com>
2016-08-16 19:12:00 -0700

    random movement

--- canvas-preview/unit.js
+++ canvas-preview/unit.js
@@ -12,6 +12,8 @@ class Unit {
         this.point = 0;
 
         this.calculateSize();
+
+        this.moveTimer = 0;
     }
 
     getRandomVel() {
@@ -32,8 +34,13 @@ class Unit {
         this.x += this.velX;
         this.y += this.velY;
 
-        //this.x = Math.min(Math.max(this.x, 0), this.canvas.width);
-        //this.y = Math.min(Math.max(this.y, 0), this.canvas.height);
+        // random movement
+        this.moveTimer--;
+        if (this.moveTimer <= 0) {
+            this.moveTimer = 30 + 30 * Math.random();        //  120 frames = 2 sec @ 60 FPS
+            this.velX = this.getRandomVel();
+            this.velY = this.getRandomVel();
+        }
     }
 
     draw(ctx) {
