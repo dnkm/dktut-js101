f6124bc79850c76ff8ce59212ccdd2e50648a84c
Daniel Kim <Daniel Kim>
2016-08-20 22:04:30 -0700

    setup 8022

---
+++ canvas-0822/game.js
@@ -0,0 +1,161 @@
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+
+    this.width = canvas.width + 100;
+    this.height = canvas.height + 100;
+
+    this.procId = -1;
+    this.units = [];
+
+    this.spawnUnits(20);
+  }
+
+  spawnUnits(num) {
+    for (var i = 0; i < num; i++) {
+      var unit = new Unit(i, this.width * Math.random(), this.height * Math.random());
+      this.units.push(unit);
+    }
+  }
+
+  start() {
+    var draw = this.draw.bind(this);
+    this.procId = setInterval(draw, 1000 / 60);
+  }
+
+  stop() {
+    clearInterval(this.procId);
+  }
+
+  draw() {
+    this.moveUnits();
+
+    // if player is dead, stop
+    if (typeof this.units[0] === 'undefined') {
+      return;
+    }
+
+    var vp = this.getViewport();
+
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.drawGrid(vp);
+    this.drawUnits(vp);
+  }
+
+  moveUnits() {
+    var that = this;
+    this.units.forEach(function (unit) {
+      unit.move();
+      that.checkCollision(unit);
+    });
+  }
+
+  checkCollision(unit) {
+    this.checkCollisionWall(unit);
+    this.checkCollisionObj(unit);
+  }
+
+  checkCollisionWall(unit) {
+    unit.x = Math.min(Math.max(unit.x, 0), this.width);
+    unit.y = Math.min(Math.max(unit.y, 0), this.height);
+
+    if (unit.x % this.width == 0) {
+      unit.velX *= -1;
+    }
+
+    if (unit.y % this.height == 0) {
+      unit.velY *= -1;
+    }
+
+  }
+
+  checkCollisionObj(unit) {
+    var that = this;
+    this.units.forEach(function (unit2) {
+      if (unit.id == unit2.id) {
+        return;
+      }
+
+      if (
+        Math.abs(unit.x - unit2.x) < unit.radius + unit2.radius
+        &&
+        Math.abs(unit.y - unit2.y) < unit.radius + unit2.radius
+      ) {
+
+        if (unit.id === 0) console.log(`collision between ${unit} & ${unit2}`);
+
+        // precise calculatio
+        var dx = Math.abs(unit.x - unit2.x);
+        var dy = Math.abs(unit.y - unit2.y);
+        var distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+        if (distance <= unit.radius + unit2.radius) {
+          if (unit.id === 0) console.log("hit!");
+          that.handleCollision(unit, unit2);
+        } else {
+          if (unit.id === 0) console.log("miss!");
+        }
+      }
+    });
+  }
+
+  handleCollision(unit, unit2) {
+    if (unit.id === 0) {
+      this.stop();
+    }
+
+    delete this.units[unit.id];
+    delete this.units[unit2.id];
+  }
+
+  getViewport() {
+    // calculate viewports
+    var p1 = this.units[0];
+    var vp = {};
+    vp.x1 = p1.x - this.canvas.width / 2;
+    vp.x1 = Math.max(Math.min(vp.x1, this.width - this.canvas.width), 0);
+    vp.x2 = vp.x1 + this.canvas.width;
+
+    vp.y1 = p1.y - this.canvas.height / 2;
+    vp.y1 = Math.max(Math.min(vp.y1, this.height - this.canvas.height), 0);
+    vp.y2 = vp.y1 + this.canvas.height;
+    return vp;
+  }
+
+  drawUnits(vp) {
+    var that = this;
+    this.units.forEach(function (unit) {
+      unit.draw(that.ctx, vp);
+    });
+
+  }
+
+  drawGrid(vp) {
+    var ctx = this.ctx;
+    ctx.strokeStyle = 'gray';
+    ctx.lineWidth = 1;
+
+    for (var y = 0; y < this.height; y += this.height / 10) {
+      ctx.beginPath();
+      ctx.moveTo(0 - vp.x1, y - vp.y1);
+      ctx.lineTo(this.width - vp.x1, y - vp.y1);
+      ctx.stroke();
+    }
+
+    for (var x = 0; x < this.width; x += this.width / 10) {
+      ctx.beginPath();
+      ctx.moveTo(x - vp.x1, 0 - vp.y1);
+      ctx.lineTo(x - vp.x1, this.height - vp.y1);
+      ctx.stroke();
+    }
+
+    ctx.fillStyle = 'gray';
+    ctx.font = '10px Arial';
+    for (var y = 0; y < this.height; y += this.height / 10) {
+      for (var x = 0; x < this.width; x += this.width / 10) {
+        ctx.fillText(`${x}`, x - vp.x1, y - vp.y1);
+      }
+    }
+
+  }
+}
---
+++ canvas-0822/unit.js
@@ -0,0 +1,69 @@
+class Unit {
+    constructor(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+
+        this.velX = this.getRandomVel();
+        this.velY = this.getRandomVel();
+        this.color = this.getRandomColor();
+
+        this.level = 1;
+        this.point = 0;
+
+        this.calculateSize();
+        this.moveTimer = 0;
+    }
+
+    getRandomVel() {
+        var plusOrMinus = (Math.random() < 0.5) ? -1 : 1;
+        return (1 + 0.5 * Math.random()) * plusOrMinus;
+    }
+
+    getRandomColor() {
+        return "#" + Math.random().toString(16).slice(2, 8).toUpperCase();
+    }
+
+    calculateSize() {
+        this.radius = 10 + (2 * (this.level - 1));
+        this.border = 0.4 + (0.2 * (this.level - 1));
+    }
+
+    move() {
+        this.x += this.velX;
+        this.y += this.velY;
+
+        // random movement
+        this.moveTimer--;
+        if (this.moveTimer <= 0) {
+            this.moveTimer = 120 + 30 * Math.random();        //  120 frames = 2 sec @ 60 FPS
+            this.velX = this.getRandomVel();
+            this.velY = this.getRandomVel();
+        }
+    }
+
+    draw(ctx, vp) {
+
+        var x = this.x - vp.x1;
+        var y = this.y - vp.y1;
+
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = this.border;
+
+        ctx.beginPath();
+        ctx.arc(x, y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+
+        if (this.id === 0) {
+            ctx.fillStyle = 'white';
+            ctx.font = "10px Arial";
+            ctx.fillText("P", x - 3, y + 3);
+        }
+    }
+
+    toString() {
+        return `(${this.id} @ (${this.x}, ${this.y}))`
+    }
+}
